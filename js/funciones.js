b066ab5141ddc1086a0328523993bc7f50b33856
Alberto García <agarciablasco2@gmail.com>
2023-04-18 16:48:24 -0400

    Implementar modal de video
    Se implementa ventana que contiene video del programa, la cual está fuera de vista. Aparece al pulsar el botón de "play".

--- js/funciones.js
+++ js/funciones.js
@@ -1,7 +1,11 @@
-var elems = ["Curso en Línea","Webinars","Talleres Presenciales","Coaching","Certificado Digital"];
-var expls = ["Aquí irá la explicación del curso en línea","Aquí irá la explicación de Webinars","Aquí irá la explicación de Talleres Presenciales","Aquí irá la explicación de Coaching","Aquí irá la explicación de Certificado Digital"];
+var elems = ["Curso en Línea","Webinars","Taller Presencial","Coaching","Certificado Digital"];
+var expls = ["Curso asincrónico donde el participante tendrá libre acceso a la base teórica y contenidos del programa: videos, tareas, casos prácticos y evaluaciones, que fortalecerán el aprendizaje.","Taller online sincrónico donde los participantes aprenden de un tema específico, interactuando y compartiendo experiencias en común.","Instancia de aprendizaje grupal donde los participantes acuden a un espacio físico y experimentan la enseñanza de manera práctica y efectiva.","Conversación profunda entre coach y coachee, donde se reflexiona acerca de diversas situaciones, se abordan propósitos y se establecen planes de acción, con la finalidad de mejorar su bienestar.","Documento digital descargable al final del proceso."];
+var player;
+
 $(document).ready(function(){
     var logoWidth = 0;
+    let iframe = document.querySelector('iframe');
+    player = new Vimeo.Player(iframe);
     /*let logoAporte = new Image(); logoAporte.src = 'img/logoAporteCol.png';
     let logoAporte_n = new Image(); logoAporte_n.src = 'img/logoAporte.png';
     $('#logoAporte').hover(function(){$(this).attr('src',logoAporte.src);},function(){$(this).attr('src',logoAporte_n.src);});*/
@@ -42,4 +46,18 @@ function enviaForm(){
 function abreExplicativo(elem){
     $("#explicacionModal").html(elems[elem]);
     $("#textoExplicativo").html(expls[elem]);
+}
+
+function slideVideo(move){
+    switch(move){
+        case 1: $("#modalVideo").removeClass("slideOut").addClass("slideIn").css("display","block");
+                $("#backVideo").css({"opacity":"0.7","z-index":"1"});
+                setTimeout(function(){player.play();},2000);
+                break;
+        case 2: player.pause();
+                $("#modalVideo").removeClass("slideIn").addClass("slideOut");
+                setTimeout(function(){$("#modalVideo").css("display","none")},2000);
+                $("#backVideo").css({"opacity":"0","z-index":"-1"});
+                break;
+    }
 }
